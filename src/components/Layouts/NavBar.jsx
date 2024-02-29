96f32f736258d99f933f6b3df3bfd631c1ce4751
adnankhayrou <adnanekhayrou3@gmail.com>
2024-02-29 15:02:00 +0100

    switching style of nav bar and side bar

--- src/components/Layouts/NavBar.jsx
+++ src/components/Layouts/NavBar.jsx
@@ -1,17 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import { React } from "react";
 import { Link } from "react-router-dom";
-import logo from "../../assets/images/gotow.svg";
+import logo from "../../assets/images/gototrip.svg";
 
 const NavBar = () => {
   return (
-    <nav className="fixed top-0 left-0 z-40 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+    <nav className="fixed top-0 left-0 z-40 w-full bg-white border-light shadow-lg">
       <div className="px-3 py-3 lg:px-5 lg:pl-3">
         <div className="flex items-center sm:justify-start justify-center sm:ms-3">
           <div className=" flex items-center justify-start rtl:justify-end">
             <Link to="" className="flex md:me-24">
               <img src={logo} width={40} alt="Logo"/>
-              {/* <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white pt-3">Go2Trip</span> */}
+              <span className="self-center text-xl  font-semibold sm:text-2xl whitespace-nowrap dark:text-black pt-3">Go2Trip</span>
             </Link>
           </div>
           {/* <div className="flex items-center">
--- src/components/Layouts/SideBar.jsx
+++ src/components/Layouts/SideBar.jsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Cookies from 'js-cookie';
+import logo from "../../assets/icons/admin.png";
+
 
 
 const SideBare = () => {
@@ -61,7 +63,7 @@ const SideBare = () => {
         }`}
         aria-label="Sidebar">
         
-        <div className="h-full px-3 pt-8 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+        <div className="h-full px-3 pt-8 py-4 overflow-y-auto bg-white dark:bg-white border-light shadow-lg">
           <div className='text-end p-0'>
             <button
             onClick={toggleSidebar}
@@ -83,16 +85,19 @@ const SideBare = () => {
             </svg>
           </button>
           </div>
-          <div className='ms-2 mt- flex items-center'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mt-4 w-10 h-10 dark:text-pink-700">
+          <div className='ms-1 mt- flex items-center'>
+          {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mt-4 w-10 h-10 dark:text-sky-500">
             <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-          </svg>
+          </svg> */}
+          <div className="mt-4 w-10 h-10 me-1">
+            <img src={logo} width={60} alt="Logo"/>
+          </div>
 
           <div className="pt-4" role="none">
-                <p className="text-sm text-gray-900 dark:text-white" role="none">
+                <p className="text-sm font-medium text-gray-900" role="none">
                 Welcome {user?.name}
                 </p>
-                <p className="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
+                <p className="text-sm font-medium text-gray-400 truncate" role="none">
                 {user?.email}
                 </p>
             </div>
@@ -100,8 +105,8 @@ const SideBare = () => {
           <ul className="ms-3 space-y-2 mt-5 font-medium items-center text-center">
 
             <li className='pt-2'>
-            <Link to="" className="mb-5 flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-pink-800 group-hover:text-gray-900 dark:group-hover:text-white">
+            <Link to="" className="mb-5 flex items-center p-2 text-black rounded-lg hover:bg-gray-200">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-sky-500 group-hover:text-gray-900 dark:group-hover:text-white">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 7.5l-2.25-1.313M21 7.5v2.25m0-2.25l-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3l2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75l2.25-1.313M12 21.75V19.5m0 2.25l-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
             </svg>
                <span className="ms-3">All Products</span>
@@ -109,8 +114,8 @@ const SideBare = () => {
             </li>
 
             <li>
-            <Link to="" className="mb-5 flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-pink-800 group-hover:text-gray-900 dark:group-hover:text-white">
+            <Link to="" className="mb-5 flex items-center p-2 text-black rounded-lg hover:bg-gray-200">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-sky-500 group-hover:text-gray-900 dark:group-hover:text-white">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21" />
             </svg>
                <span className="ms-3">Your Products</span>
@@ -118,8 +123,8 @@ const SideBare = () => {
             </li>
 
             <li>
-            <Link to="" className="mb-5 flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-pink-800 group-hover:text-gray-900 dark:group-hover:text-white">
+            <Link to="" className="mb-5 flex items-center p-2 text-black rounded-lg hover:bg-gray-200">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-sky-500 group-hover:text-gray-900 dark:group-hover:text-white">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
             </svg>
                <span className="ms-3">Categories</span>
@@ -127,8 +132,8 @@ const SideBare = () => {
             </li>
 
             <li>
-            <Link to="" className="mb-5 flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-pink-800 group-hover:text-gray-900 dark:group-hover:text-white">
+            <Link to="" className="mb-5 flex items-center p-2 text-black rounded-lg hover:bg-gray-200">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-sky-500 group-hover:text-gray-900 dark:group-hover:text-white">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
             </svg>
                <span className="ms-3">Sub Categories</span>
@@ -136,8 +141,8 @@ const SideBare = () => {
             </li>
 
             <li>
-            <Link to="" className="mb-5 flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-pink-800 group-hover:text-gray-900 dark:group-hover:text-white">
+            <Link to="" className="mb-5 flex items-center p-2 text-black rounded-lg hover:bg-gray-200">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-sky-500 group-hover:text-gray-900 dark:group-hover:text-white">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
             </svg>
                <span className="ms-3">Comments</span>
@@ -145,8 +150,8 @@ const SideBare = () => {
             </li>
 
             <li>
-            <Link to="" className="mb-5 flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-pink-800 group-hover:text-gray-900 dark:group-hover:text-white">
+            <Link to="" className="mb-5 flex items-center p-2 text-black rounded-lg hover:bg-gray-200">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-sky-500 group-hover:text-gray-900 dark:group-hover:text-white">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
             </svg>
                <span className="ms-3">All Users</span>
@@ -154,8 +159,8 @@ const SideBare = () => {
             </li>
 
             <li>
-            <Link to="" className="mb-5 flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-pink-800 group-hover:text-gray-900 dark:group-hover:text-white">
+            <Link to="" className="mb-5 flex items-center p-2 text-black rounded-lg hover:bg-gray-200">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-sky-500 group-hover:text-gray-900 dark:group-hover:text-white">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
             </svg>
                <span className="ms-3">Cities</span>
@@ -163,8 +168,8 @@ const SideBare = () => {
             </li>
             
             <li>
-                <Link to="/forgotPassword" className="mb-5 flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-pink-800 group-hover:text-gray-900 dark:group-hover:text-white">
+                <Link to="/forgotPassword" className="mb-5 flex items-center p-2 text-black rounded-lg hover:bg-gray-200">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-sky-500 group-hover:text-gray-900 dark:group-hover:text-white">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
                 </svg>
                     <span className="ms-3">Reset Password</span>
@@ -172,8 +177,8 @@ const SideBare = () => {
             </li>
 
             <li>
-                <Link onClick={logout} className="mb-5 flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-pink-800 group-hover:text-gray-900 dark:group-hover:text-white">
+                <Link onClick={logout} className="mb-5 flex items-center p-2 text-black rounded-lg hover:bg-gray-200">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-gray-500 transition duration-75 dark:text-sky-500 group-hover:text-gray-900 dark:group-hover:text-white">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
                 </svg>
 
