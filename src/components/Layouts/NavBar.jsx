a2cca5371b4117a43751cd64d7075dd587e370a0
adnankhayrou <adnanekhayrou3@gmail.com>
2024-03-04 12:01:34 +0100

    changing navbar style

--- src/components/Layouts/NavBar.jsx
+++ src/components/Layouts/NavBar.jsx
@@ -1,17 +1,42 @@
-// eslint-disable-next-line no-unused-vars
-import { React } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/gototrip.svg";
 
 const NavBar = () => {
+  const [bgColor, setBgColor] = useState("transparent");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setBgColor("rgba(255, 255, 255, 0.5)");
+      } else {
+        setBgColor("whitesmoke");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="fixed top-0 left-0 z-40 w-full bg-white border-light shadow-2xl">
+    <nav
+      id="nav"
+      className="fixed top-0 left-0 right-0 z-40 bg-white border-light shadow-2xl rounded-2xl m-4"
+      style={{
+        background: bgColor,
+      }}
+    >
       <div className="px-3 py-3 lg:px-5 lg:pl-3">
         <div className="flex items-center sm:justify-start justify-center sm:ms-3">
-          <div className=" flex items-center justify-start rtl:justify-end">
+          <div className="flex items-center justify-start rtl:justify-end">
             <Link to="" className="flex md:me-24">
-              <img src={logo} width={40} alt="Logo"/>
-              <span className="self-center font-bold sm:text-2xl whitespace-nowrap dark:text-gray-500 pt-3">Go2Trip</span>
+              <img src={logo} width={40} alt="Logo" />
+              <span className="self-center font-bold sm:text-2xl whitespace-nowrap dark:text-gray-500 pt-3">
+                Go2Trip
+              </span>
             </Link>
           </div>
           {/* <div className="flex items-center">
