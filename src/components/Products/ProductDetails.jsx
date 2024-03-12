cdeea7b7b49569ff64a43f47842264f4606302af
adnankhayrou <adnanekhayrou3@gmail.com>
2024-03-12 10:22:22 +0000

    adding whatsapp api to make user send message to seller directly from product details component

--- src/components/Products/ProductDetails.jsx
+++ src/components/Products/ProductDetails.jsx
@@ -65,8 +65,8 @@ const ProductDetails = () => {
         {/* back button */}
         <div className="container w-3/4 items-center rounded-lg mb-3 me-3 ">
           <Link to="/dashboard" className="text-decoration-none text-black">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
             </svg>
           </Link>
         </div>
@@ -77,7 +77,7 @@ const ProductDetails = () => {
               {product.images && product.images.length > 0 && (
               <div className="relative h-56 overflow-hidden rounded-lg md:h-96 ">
                   {product.images.map((image, index) => (
-                    <div  className={`image-slide ${index === currentImageIndex ? 'block' : 'hidden'}`}>
+                    <div key={index}  className={`image-slide ${index === currentImageIndex ? 'block' : 'hidden'}`}>
                       <img  src={`http://localhost:3000${image}`}
                             alt={`Slide ${index}`}
                             className="absolute inset-0 rounded w-max h-full object-cover sm:ms-[280px] ms-[47px]" />
@@ -116,12 +116,12 @@ const ProductDetails = () => {
                 </div>
 
                 <div className="mx-3 mt-5 flex items-center text-gray-400 ">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mb-0.5 me-1">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 mb-0.5 me-1">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                   </svg>
                   <p className=""> agadir - </p>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mx-1">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 mx-1">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                   </svg>
                   <p className="">listed date</p>
                 </div>
@@ -133,12 +133,12 @@ const ProductDetails = () => {
                 <div className='sm:flex items-center my-5 justify-between'>
                   <div className="mx-3 flex items-center text-gray-600">
                     <p>Category</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-4">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-4">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
                     </svg>
                     <p>{product.category_id?.name}</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-4">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-4">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
                     </svg>
                     <p>{product.subCategory_id?.name}</p>
                   </div>
@@ -147,17 +147,25 @@ const ProductDetails = () => {
                   <hr className="my-3" />
                   </div>
 
-                  <div className='sm:flex text-center me-2 pt-2 sm:pt-0'>
-                    <div className="col-12 col-md-6 col-lg-6 border rounded me-2">
-                      <p className="text-light">
-                        <i className="bi bi-phone"></i>
+                  <div className='flex text-center justify-end pt-2 sm:pt-0'>
+                    <div className="bg-black border rounded-md me-2 py-1 px-2">
+                      <p className="text-white flex items-center">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
+                      </svg>
                       <a href={"tel:" + product.phone}>{product.phone}</a> 
                       </p>
                     </div>
-                    <div className="col-12 col-md-6 col-lg-6 rounded border">
-                      <p className="text-light">
-                        <i className="bi bi-phone"></i>
-                      <a href={"tel:" + product.phone}>{product.phone}</a> 
+
+                    <div className="bg-green-700 border rounded-md me-2 py-1 px-2">
+                      <p className="text-white flex items-center">
+                      <svg viewBox="0 0 56.693 56.693" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 56.693 56.693" className='h-6 w-6 me-1'>
+                        <path d="M46.38 10.714C41.73 6.057 35.544 3.492 28.954 3.489c-13.579 0-24.63 11.05-24.636 24.633a24.589 24.589 0 0 0 3.289 12.316L4.112 53.204l13.06-3.426a24.614 24.614 0 0 0 11.772 2.999h.01c13.577 0 24.63-11.052 24.635-24.635.002-6.582-2.558-12.772-7.209-17.428zM28.954 48.616h-.009a20.445 20.445 0 0 1-10.421-2.854l-.748-.444-7.75 2.033 2.07-7.555-.488-.775a20.427 20.427 0 0 1-3.13-10.897c.004-11.29 9.19-20.474 20.484-20.474a20.336 20.336 0 0 1 14.476 6.005 20.352 20.352 0 0 1 5.991 14.485c-.004 11.29-9.19 20.476-20.475 20.476z" fillRule="evenodd" clipRule="evenodd" fill="#ffffff" className="fill-000000"/>
+                        <path d="M40.185 33.281c-.615-.308-3.642-1.797-4.206-2.003-.564-.205-.975-.308-1.385.308-.41.617-1.59 2.003-1.949 2.414-.359.41-.718.462-1.334.154-.615-.308-2.599-.958-4.95-3.055-1.83-1.632-3.065-3.648-3.424-4.264-.36-.617-.038-.95.27-1.257.277-.276.615-.719.923-1.078.308-.36.41-.616.616-1.027.205-.41.102-.77-.052-1.078-.153-.308-1.384-3.338-1.897-4.57-.5-1.2-1.008-1.038-1.385-1.057-.359-.018-.77-.022-1.18-.022s-1.077.154-1.642.77c-.564.616-2.154 2.106-2.154 5.135 0 3.03 2.206 5.957 2.513 6.368.308.41 4.341 6.628 10.516 9.294a35.341 35.341 0 0 0 3.509 1.297c1.474.469 2.816.402 3.877.244 1.183-.177 3.642-1.49 4.155-2.927.513-1.438.513-2.67.359-2.927-.154-.257-.564-.41-1.18-.719z" fillRule="evenodd" clipRule="evenodd" fill="#ffffff" className="fill-000000"/>
+                      </svg>
+                      <a target="_blank" href={`https://wa.me/${product.telephone}?text=Hello ${product.user_id.name}, is this item "${product.name}" still available on Go2Trip market?`}>
+                        Contact Seller
+                      </a>                      
                       </p>
                     </div>
                   </div>
